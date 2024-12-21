1cf9e762ca542f6e5513c751e2278f604c0b12e2
leonardo fleitas <leoflalv.dev@gmail.com>
2024-12-21 16:18:54 +0100

    create orders project

---
+++ src/config/envs.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z
+  .object({
+    PORT: z.preprocess((val) => Number(val), z.number().int().positive()),
+  })
+  .passthrough();
+
+type EnvsVars = z.infer<typeof envSchema>;
+
+const { error, data } = envSchema.safeParse(process.env);
+
+if (error) {
+  throw new Error(`Invalid environment variables: ${error.message}`);
+}
+
+export const envs: EnvsVars = data;
